2843b5456c26d1583a93b0194dfa02fe31820519
unknown <alexandra.espindola@valtech.com>
2020-02-26 11:10:00 -0300

    adicionando ultimos exercicios - aula 4

--- js/principal.js
+++ js/principal.js
@@ -1,35 +1,60 @@
-//console.log("fui carregado corretamente")
-// var titulo = document.querySelector("h1");
-// console.log(titulo)
-// console.log(titulo.textContent)
-// titulo.textContent = "Aparecida Nutricionista";
-
 var titulo = document.querySelector(".titulo")
-titulo.textContent = "help :'("
+titulo.textContent = "Aparecida Nutricionista"
 
 var paciente = document.querySelector("#primeiro-paciente")
 
-var tdPeso = paciente.querySelector(".info-peso")
-var peso = tdPeso.textContent // deu o mesmo resultado sem a variavel peso. Acredito que é pra ficar mais organi
-//zado, e não ter que escrever tanto
-var tdAltura = paciente.querySelector(".info-altura")
-var altura = tdAltura.textContent
+var pacientes = document.querySelectorAll(".paciente")
 
-// console.log(paciente) //aqui tem que conter o tr (paciente)
-// console.log(tdPeso) //aqui tem que conter o td (peso)
-// console.log(peso)
-// console.log(altura)
-// //console.log(tdPeso.textContent) deu certo sem a variavel peso
 
-var tdImc = paciente.querySelector(".info-imc")
+for (let index = 0; index < pacientes.length; index++) {
 
-if(peso < 0 || peso > 600) {
-   console.log("Peso inválido") 
+   var paciente = pacientes[index];
+
+
+   var tdPeso = paciente.querySelector(".info-peso")
+   var peso = tdPeso.textContent
+   var tdAltura = paciente.querySelector(".info-altura")
+   var altura = tdAltura.textContent
+
+   var tdImc = paciente.querySelector(".info-imc")
+
+   var alturaValida = true;
+   var pesoValido = true;
+
+
+   if (peso <= 0 || peso >= 600) {
+      console.log("Peso inválido")
+      pesoValido = false;
+      tdImc.textContent = "Peso inválido";
+      paciente.classList.add("paciente-invalido");
+   } else {
+      console.log("peso válido")
+   }
+
+   if (altura <= 0 || altura >= 3.0) {
+      console.log("altura inválida")
+      alturaValida = false;
+      tdImc.textContent = "Altura inválida";
+      paciente.classList.add("paciente-invalido");
+   } else {
+      console.log("altura valida")
+   }
+
+   if (alturaValida && pesoValido) {
+      var imc = peso / (altura * altura)
+      console.log("o valor do imc é: " + imc)
+      tdImc.textContent = imc.toFixed(2);//coloca o valor resultande de imc e joga dentro da tdImc
+
+   } else {
+      tdImc.textContent = "Altura e/ou Peso Inválidos!"
+      console.log("Altura e/ou Peso Inválidos!")
+   }
+
 }
 
-if(altura < 0 || altura > 3.0)
+var btnAdicionar = document.querySelector("#adicionar-paciente");
+console.log(btnAdicionar);
 
-var imc = peso/(altura*altura)
-console.log(imc)
-
-tdImc.textContent = imc;//coloca o valor resultande de imc e joga dentro da tdImc
+btnAdicionar.addEventListener("click", function () {
+   console.log("fui clicado");
+})
