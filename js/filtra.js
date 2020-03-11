001d8f8419174d9c765dc4f2dbf8958bdbac950f
Alexandra Espíndola <ale.espindola.silva@gmail.com>
2020-03-11 18:14:47 -0300

    atualização exercicios js

---
+++ js/filtra.js
@@ -0,0 +1,9 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+campoFiltro.addEventListener("input", function(){
+    // console.log("Digitaram no campo");
+    // console.log(campoFiltro.value);
+    console.log(this.value);
+    var pacientes = document.querySelectorAll(".pacientes");
+
+})
