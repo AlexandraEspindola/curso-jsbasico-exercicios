aa3da4feaa6e72b0aa44513b54db648ce3e96946
Alexandra <ale.espindola.silva@gmail.com>
2020-03-12 12:20:49 -0300

    Finalização filtro aula 8

--- js/filtra.js
+++ js/filtra.js
@@ -11,9 +11,10 @@ campoFiltro.addEventListener("input", function () {
             var paciente = pacientes[i];
             var tdNome = paciente.querySelector(".info-nome");
             var nome = tdNome.textContent;
-            console.log(nome);
+            var expressao = new RegExp(this.value, "i");
+            //console.log(nome);
 
-            if (nome != this.value) {
+            if (!expressao.test(nome)) {
                 paciente.classList.add("invisivel");
             } else {
                 paciente.classList.remove("invisivel");
